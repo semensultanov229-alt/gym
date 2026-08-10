bf4b0baf22921b23035e8f543d03c457f85b34e5
BOSS OF THE GYM <semensultanov229@gmail.com>
2026-08-10 22:48:06 +0500

    update: create for first lvl

---
+++ complete/day00.py
@@ -0,0 +1,11 @@
+# 1. Создаем переменные
+my_name = "Семён"
+my_age = 20
+pi_value= 3.14159
+likes_programming = True
+
+# 2. Узнаем типы и выводим их в терминал
+print(type(my_name))
+print(type(my_age))
+print(type(pi_value))
+print(type(likes_programming))
